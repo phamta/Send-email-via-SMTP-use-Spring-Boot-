f06503796d64494222cc11bca02ad0d34d77f93c
Tanvan <tanvan.13022014@gmail.com>
2025-04-17 20:52:39 +0700

    First commit

---
+++ src/main/java/com/tanvan/emailapplication/controller/EmailController.java
@@ -0,0 +1,36 @@
+package com.tanvan.emailapplication.controller;
+
+import com.tanvan.emailapplication.entity.EmailDetails;
+import com.tanvan.emailapplication.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+    @Autowired
+    private EmailService emailService;
+
+    // Sending a simple Email
+    @PostMapping("/sendMail")
+    public String
+    sendMail(@RequestBody EmailDetails details)
+    {
+        String status
+                = emailService.sendSimpleMail(details);
+
+        return status;
+    }
+
+    // Sending email with attachment
+    @PostMapping("/sendMailWithAttachment")
+    public String sendMailWithAttachment(
+            @RequestBody EmailDetails details)
+    {
+        String status
+                = emailService.sendMailWithAttachment(details);
+
+        return status;
+    }
+}
